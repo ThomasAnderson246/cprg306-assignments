698c91eea677e4aeba850bac1d03133d81c7fdba
Thomas Anderson <roocifer246@gmail.com>
2025-05-13 10:24:26 -0600

    Week 2 Assignment

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -1,9 +1,13 @@
+import Link from "next/link";
 
 
 export default function StudentInfo(){
     return(
         <div>
-            
+            <ul>
+                <li>Thomas Anderson</li>
+                <li><Link href="https://github.com/ThomasAnderson246/cprg306-assignments">GitHub Link</Link></li>
+            </ul>
         </div>
     );
 }
