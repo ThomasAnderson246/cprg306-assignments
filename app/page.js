d04e776714e7875f2b21ce4c52872404f864652c
Thomas Anderson <roocifer246@gmail.com>
2025-05-26 23:41:34 -0600

    Mild styling

--- app/page.js
+++ app/page.js
@@ -2,8 +2,10 @@ import Link from "next/link";
 
 
 export default function Home() {
+
+  let linkStyle = "";
   return (
-   <main>
+   <main className="bg-amber-300">
     <h1>CPRG 306: Web Development 2 - Assignments</h1>
     <p><Link href="week-2">Week 2 Assignment</Link></p>
     <p><Link href="week-3">Week 3 Assignment</Link></p>
