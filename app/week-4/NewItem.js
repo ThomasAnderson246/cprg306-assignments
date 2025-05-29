18b2240df4f0b173567c065322845ae6add785b1
Thomas Anderson <roocifer246@gmail.com>
2025-05-29 12:43:52 -0600

    Disabled Functionality Working
    Still need to finish styling.

--- app/week-4/NewItem.js
+++ app/week-4/NewItem.js
@@ -13,10 +13,14 @@ export default function NewItem(){
         setCount(count-1);
     }
 
-    let buttonStyle = "bg-green-500 w-20 rounded active:bg-amber-400";
+    let buttonStyle = "bg-green-500 w-20 rounded active:bg-amber-400 disabled:bg-gray-500";
     return (
         <div>
-            <p>{count} <button className={buttonStyle} onClick={increment}>+</button> <button className={buttonStyle} onClick={decrement}>-</button></p>
+            <p>
+                {count} 
+                <button className={buttonStyle} onClick={increment} disabled={count===20}>+</button> 
+                <button className={buttonStyle} onClick={decrement} disabled={count===1}>-</button>
+            </p>
         </div>
     );
 }
