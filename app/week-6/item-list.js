79af7af972fca9249b30970fddc6dbe9794e3d90
Thomas Anderson <roocifer246@gmail.com>
2025-06-15 22:04:32 -0600

    Update item-list.js

--- app/week-6/item-list.js
+++ app/week-6/item-list.js
@@ -23,20 +23,26 @@ export default function ItemList(){
     
     return(
 
-        
-        <div>
-          <div>
-            <label>Sort By:</label>
-            <select onChange={handleSortChange}>
-              <option value="name">Item Name</option>
-              <option value="category">Category</option>
-            </select>
-          </div>
-            <div>
-              {itemArray.map((item) => (
-                <Item key={item.id} itemObj={item}/>
-               ))}
+        <div className="flex flex-col md:flex-row p-w gap-8 items-center">
+          <div className="md:w-1/4 flex justify-center">
+            <div className="bg-green-600 p-4 rounded-lg mb-4 border border-gray-200">
+              <label className="block text-amber-300 text-lg font-bold mb-2">Sort By:</label>
+              <select 
+              onChange={handleSortChange}
+              className="block bg-amber-300 w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 ">
+                <option value="name">Item Name</option>
+                <option value="category">Category</option>
+              </select>
             </div>
-        </div>
+            </div>
+              <div className="md:w-3/4">
+              <ul className="grid grid-cols-1 sm:grid-cols-2 lg_grid-cols-3 gap-4">
+                {itemArray.map((item) => (
+                  <Item key={item.id} itemObj={item}/>
+                ))}
+                </ul>
+              </div>
+          </div>
+        
     );
 }
