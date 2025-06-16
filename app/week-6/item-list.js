bf049f27c2b11ac2aed4af21b8c23bd340992111
Thomas Anderson <roocifer246@gmail.com>
2025-06-15 20:22:28 -0600

    item-list updated

--- app/week-6/item-list.js
+++ app/week-6/item-list.js
@@ -8,14 +8,35 @@ export default function ItemList(){
 
   const [sortBy, setSortBy] = useState("name");
 
-  let itemArray = itemList.map( (item) => [{...item}]);
+  let itemArray = itemList.map( (item) => ({...item}));
+
+  const handleSortChange = (event) => setSortBy(event.target.value);
+
+  itemArray.sort((a,b) => {
+    let itemA = a[sortBy].toUpperCase();
+    let itemB = b[sortBy].toUpperCase();
+    if(itemA < itemB) return -1
+    if(itemA > itemB) return 1;
+    return 0;
+  })
 
     
     return(
+
+        
         <div>
-            {itemArray.map((item) => (
-              <Item key={item.id} itemObj={item}/>
-            ))}
+          <div>
+            <label>Sort By:</label>
+            <select onChange={handleSortChange}>
+              <option value="name">Item Name</option>
+              <option value="category">Category</option>
+            </select>
+          </div>
+            <div>
+              {itemArray.map((item) => (
+                <Item key={item.id} itemObj={item}/>
+               ))}
+            </div>
         </div>
     );
 }
